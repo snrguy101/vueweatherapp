1f5607bf4679c81ff7219d0617bad66e2dc96369
Charles Umeanakor <codesngr@gmail.com>
2022-11-30 08:51:33 +0100

    fixed styling

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -1,12 +1,7 @@
 <template>
-    <div>
+   
         <div class="flex flex-col flex-1 items-center">
-            <div class="text-white p-4 bg-weather-secondary w-full text-center">
-                <p>You are currently previewing this city</p>
-            </div>
-        </div>
-    
-        <div class="flex flex-col items-center text-white py-12">
+            <div class="flex flex-col items-center text-white py-12">
             <h1 class="text-4xl mb-2">{{route.params.city}}</h1>
             <p class="text-sm mb-12">
                 {{ new Date(weatherData.currentTime).toLocaleDateString( "en-us", { weekday: "short", day: "2-digit", month: "long", } ) }} {{ new Date(weatherData.currentTime).toLocaleTimeString( "en-us", { timeStyle: "short", } ) }}
@@ -78,7 +73,10 @@
             <p>Remove City</p>
         </div>
     
-    </div>
+        </div>
+    
+    
+
 </template>
 
 <script setup>
